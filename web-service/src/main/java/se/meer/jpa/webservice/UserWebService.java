8167d4de560c5ecad23a8dd346f4a407bb8e829b
Robin Brandt <robinbrandt81@gmail.com>
2015-09-03 15:13:02 +0200

    some methods in jaxrs done, added dependency for gson

--- web-service/src/main/java/se/meer/jpa/webservice/UserWebService.java
+++ web-service/src/main/java/se/meer/jpa/webservice/UserWebService.java
@@ -1,11 +1,15 @@
 package se.meer.jpa.webservice;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -26,15 +30,34 @@ public class UserWebService
 	private AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 
 
+//	@GET
+//	@Path("{teamName}")
+//	@Produces(MediaType.TEXT_PLAIN)
+//	public Response findUserByTeam(@PathParam("teamName") final String teamName)
+//	{
+//		final ArrayList<User> usersInTeam = getUserService().findUsersByTeam(teamName);
+//		return Response.ok().entity(usersInTeam).build();
+//	}
+//	
+//	@GET
+//	@Path("{firstname}")
+//	@Produces(MediaType.TEXT_PLAIN)
+//	public Response findUserByFirstname(@PathParam("firstname") final String firstname)
+//	{
+//		final ArrayList<User> users = getUserService().findUserByFirstname(firstname);
+//		return Response.ok().entity(users).build();
+//	}
+	
 	@GET
-	// @Path("{userId}")
+	@Path("{id}")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String sayHello()
+	public Response findUserById(@PathParam("id") final Long id)
 	{
-		User user = new User("Berit", "lastname", "emp");
-		getUserService().addUser(user);
-		return user.getFirstname();
+		//final ArrayList<User>
+		final User user = getUserService().findUserById(id);
+		return Response.ok().entity(user.getId()).build();
 	}
+	
 
 	public UserService getUserService()
 	{
