c22681a9f6464643ee506a987e64535c02e6f88b
Eric Lantz <elantz12@gmail.com>
2015-09-11 09:14:51 +0200

    Fixed last minute formatting

--- spring-data-jpa/src/main/java/se/meer/jpa/service/UserService.java
+++ spring-data-jpa/src/main/java/se/meer/jpa/service/UserService.java
@@ -10,65 +10,54 @@ import se.meer.jpa.model.Team;
 import se.meer.jpa.model.User;
 import se.meer.jpa.repository.UserRepository;
 
-
-public class UserService
-{
+public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
 
 	@Transactional
-	public User createOrUpdateUser(User user)
-	{
+	public User createOrUpdateUser(User user) {
 		return userRepository.save(user);
 	}
 
-	public User findUserById(Long id)
-	{
+	public User findUserById(Long id) {
 		return userRepository.findOne(id);
 	}
 
-	public List<User> findUserByFirstname(String firstname)
-	{
+	public List<User> findUserByFirstname(String firstname) {
 		return userRepository.findByFirstname(firstname);
 	}
 
-	public List<User> findUserByLastname(String lastname)
-	{
+	public List<User> findUserByLastname(String lastname) {
 		return userRepository.findByLastname(lastname);
 	}
 
-	public List<User> findUserByUsername(String username)
-	{
+	public List<User> findUserByUsername(String username) {
 		return userRepository.findByUsername(username);
 	}
-	
+
 	public User findUserByUserNo(String userNumber) {
 		return userRepository.findUserByUserNumber(userNumber);
 	}
 
-	public List<User> findUsersByTeam(Team team)
-	{
+	public List<User> findUsersByTeam(Team team) {
 		List<User> users = new ArrayList<>();
 		users.addAll(userRepository.findUserByTeam(team));
 		return users;
 	}
 
-	public List<User> findUsersByTeamId(Long id)
-	{
+	public List<User> findUsersByTeamId(Long id) {
 		List<User> users = new ArrayList<>();
 		users.addAll(userRepository.findUserByTeamId(id));
 		return users;
 	}
 
-	public Long deleteUserById(Long id)
-	{
+	public Long deleteUserById(Long id) {
 		userRepository.delete(id);
 		return id;
 	}
 
-	public User deleteUser(User user)
-	{
+	public User deleteUser(User user) {
 		userRepository.delete(user);
 		return user;
 	}
