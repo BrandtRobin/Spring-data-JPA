65e4e3ac0ed4d24d279c277d7ebd6530fe441076
Robin Brandt <robinbrandt81@gmail.com>
2015-10-13 14:43:48 +0200

    user fix

--- spring-data-jpa/src/main/java/se/meer/jpa/model/User.java
+++ spring-data-jpa/src/main/java/se/meer/jpa/model/User.java
@@ -34,7 +34,7 @@ public class User extends AbstractEntity {
 	@JoinColumn(name = "team_id")
 	private Team team;
 
-	@OneToMany(mappedBy = "users", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
 	private Collection<WorkItem> workItems;
 
 	protected User() {
