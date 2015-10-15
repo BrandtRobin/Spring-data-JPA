13be76132accad5b6e4de43d9b23248f4ed64ea0
Eric Lantz <elantz12@gmail.com>
2015-10-15 21:35:14 +0200

    Added cred and token model

--- web-service/src/main/java/se/meer/jpa/webservice/LoginWebService.java
+++ web-service/src/main/java/se/meer/jpa/webservice/LoginWebService.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.Random;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -19,6 +18,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import se.meer.jpa.Hash;
 import se.meer.jpa.model.Credentials;
+import se.meer.jpa.model.Token;
+import se.meer.jpa.model.User;
 import se.meer.jpa.service.UserService;
 
 @Path("login")
@@ -29,10 +30,10 @@ public class LoginWebService {
 	@Context
 	private UriInfo uriInfo;
 
-	private static final HashMap<String, String> tokenMap = new HashMap<String, String>();
-
 	private static final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
 	private static UserService service = new UserService();
+
+	private static HashMap<String, Token> tokenMap = new HashMap<>();
 
 	static {
 		context.scan("se.meer.jpa.config");
@@ -42,19 +43,19 @@ public class LoginWebService {
 
 	@POST
 	public Response authenticateUser(final Credentials loginUser) {
-
+		
+		User user = service.findUserByUsername(loginUser.getUsername());
 		System.out.println("PASSWORD " + service.findUserByUsername(loginUser.getUsername()).getPassword());
+		
 		try {
-
-			Hash.validate(loginUser.getPassword(), service.findUserByUsername(loginUser.getUsername()).getPassword());
-
-			String token = Hash.createHash(createTokenString());
+			Hash.validate(loginUser.getPassword(), user.getPassword());
+			Token token = new Token(Hash.createHash(createTokenString()));
 			tokenMap.put(loginUser.getUsername(), token);
+			return Response.ok().entity(token).build();
 			
-			return Response.ok(token).build();
 
 		} catch (Exception e) {
-			return Response.status(Response.Status.UNAUTHORIZED).build();
+			return Response.ok("Hejsan").build();
 		}
 	}
 	
